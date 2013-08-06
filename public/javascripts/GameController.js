b97cdcb626cc138e0806381bb37d09f18950c1e6
msemenistyi <nikeiwe@gmail.com>
2013-08-06 22:40:21 +0300

    started implementing player names

--- public/javascripts/GameController.js
+++ public/javascripts/GameController.js
@@ -3,28 +3,52 @@ define(function(require){
 
 	var enums = require('shared/enums'),
 		mediator = require('libs/mediator'),
-		Game = require('Game');
-		require = require('ViewManager');
+		Game = require('Game'),
+		mediator = require('libs/mediator');
+		// require = require('ViewManager');
 
 	function GameController(){
 
 		var el_ = {};
 		el_.switchButton = $('#local-network-switcher');
 		el_.newGameButton = $('#start-game-button');
+		el_.player1Name = $('#player1-name');
+		el_.player2Name = $('#player2-name');
 
-		el_.switchButton.on('change', onSwitch_);
-		el_.newGameButton.on('click', newGame_);
+		var names = {};
+		
+
+		function bindListeners_(){
+			el_.switchButton.on('change', onSwitch_);
+			el_.newGameButton.on('click', newGame_);
+			$('.player-name').on('focus', function(){
+				var container = $(this);
+				container.val('');
+			});
+			$('.player-name').on('blur', function(){
+				var container = $(this);
+				if (container.val().length === 0){
+					container.val('player');
+				}
+			});
+		}
+
+		bindListeners_();
 
 		var localGame_ = false;
 
 		function onSwitch_(item){
 			localGame_ = !localGame_;
+			el_.player2Name.toggle();
 		} 
 
 		function newGame_(){
 			var options = {
 				isLocal: localGame_
 			};
+			if (!localGame_){
+				mediator.publish('game-controller:new', {player1: })
+			}
 			new Game(options);
 		}
 	}
